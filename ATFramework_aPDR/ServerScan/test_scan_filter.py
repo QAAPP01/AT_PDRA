ea97f4370a69a6fbcc2c1f6a9ceeaa8c22eb96b3
Hausen_Lin <hausen>
2024-05-20 13:43:35 +0800

    Add scan filter & sticker

---
+++ ATFramework_aPDR/ServerScan/test_scan_filter.py
@@ -0,0 +1,52 @@
+import traceback
+import inspect
+import pytest
+import allure
+
+from ATFramework_aPDR.ATFramework.utils.compare_Mac import HCompareImg
+from ATFramework_aPDR.ATFramework.utils.log import logger
+from ATFramework_aPDR.pages.locator import locator as L
+
+from .conftest import TEST_MATERIAL_FOLDER as test_material_folder
+video_9_16 = 'video_9_16.mp4'
+video_16_9 = 'video_16_9.mp4'
+photo_9_16 = 'photo_9_16.jpg'
+photo_16_9 = 'photo_16_9.jpg'
+
+
+@allure.feature("Scan Filter")
+class Test_Scan_Filter:
+    @pytest.fixture(autouse=True)
+    def init_shortcut(self, shortcut):
+        self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
+
+        self.click = self.page_main.h_click
+        self.long_press = self.page_main.h_long_press
+        self.element = self.page_main.h_get_element
+        self.elements = self.page_main.h_get_elements
+        self.is_exist = self.page_main.h_is_exist
+        self.is_not_exist = self.page_main.h_is_not_exist
+
+    @allure.story("Filter Download")
+    def test_filter_download(self, driver):
+        func_name = inspect.stack()[0][3]
+        logger(f"\n[Start] {func_name}")
+
+        try:
+            self.page_main.enter_launcher()
+            self.page_main.enter_timeline()
+            self.page_edit.add_master_media('photo', test_material_folder, photo_9_16)
+            before = self.page_edit.get_preview_pic()
+            self.page_edit.click_tool("Filter")
+            self.click(L.edit.filter.filter())
+            self.click(L.edit.try_before_buy.try_it_first, 1)
+            self.page_edit.waiting_download()
+            after = self.page_edit.get_preview_pic()
+
+            assert not HCompareImg(before, after).ssim_compare()
+
+        except Exception:
+            traceback.print_exc()
+            driver.driver.close_app()
+
+            assert Exception
---
+++ ATFramework_aPDR/ServerScan/test_scan_sticker.py
@@ -0,0 +1,43 @@
+import traceback
+import inspect
+import pytest
+import allure
+
+from ATFramework_aPDR.ATFramework.utils.log import logger
+from ATFramework_aPDR.pages.locator import locator as L
+
+
+@allure.feature("Scan Sticker")
+class Test_Scan_Sticker:
+    @pytest.fixture(autouse=True)
+    def init_shortcut(self, shortcut):
+        self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
+
+        self.click = self.page_main.h_click
+        self.long_press = self.page_main.h_long_press
+        self.element = self.page_main.h_get_element
+        self.elements = self.page_main.h_get_elements
+        self.is_exist = self.page_main.h_is_exist
+        self.is_not_exist = self.page_main.h_is_not_exist
+
+    @allure.story("Sticker Download")
+    def test_sticker_download(self, driver):
+        func_name = inspect.stack()[0][3]
+        logger(f"\n[Start] {func_name}")
+
+        try:
+            self.page_main.enter_launcher()
+            self.page_main.enter_timeline()
+            self.page_edit.enter_sticker_library('sticker')
+            self.click(L.edit.sticker.sticker())
+            self.page_edit.waiting_download()
+
+            assert self.is_exist(L.edit.preview.pip_preview)
+            self.click(L.edit.sticker.close)
+            self.click(L.edit.menu.delete)
+
+        except Exception:
+            traceback.print_exc()
+            driver.driver.close_app()
+
+            raise Exception
