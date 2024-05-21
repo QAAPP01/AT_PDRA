d3a767494ad0f41b0cf445af0b5281e6cfd9eda0
yukino0113 <jethro0113@gmail.com>
2024-05-21 11:00:25 +0800

    Complete audio track limitation
    1. Complete audio track limitation test
    2. Add template fixture due to SFT/conftest.py has updated
    3. Change some driver action calling to use fixture actions

--- ATFramework_aPDR/SFT/Test_Timeline/test_TrackLimitation.py
+++ ATFramework_aPDR/SFT/Test_Timeline/test_TrackLimitation.py
@@ -1,8 +1,7 @@
-import time
-
 import pytest
 import allure
 
+from ATFramework_aPDR.ATFramework.utils import logger
 from ATFramework_aPDR.SFT.Test_Timeline.TestBase import TestBase
 import ATFramework_aPDR.pages.locator as L
 from ATFramework_aPDR.ATFramework.drivers.appium_driver import AppiumU2Driver
@@ -18,49 +17,84 @@ allure levels:
 
 @allure.feature('Track Limitation')
 class TestTrackLimitation(TestBase):
+
+    @pytest.fixture(autouse=True)
+    def initial(self, shortcut):
+        # shortcut
+        self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
+
+        self.click = self.page_main.h_click
+        self.long_press = self.page_main.h_long_press
+        self.element = self.page_main.h_get_element
+        self.elements = self.page_main.h_get_elements
+        self.is_exist = self.page_main.h_is_exist
+        self.is_not_exist = self.page_main.h_is_not_exist
+
     VIDEO_LIMITATION = 3
     AUDIO_LIMITATION = 9
     PIP_LIMITATION = 9
 
-    """
-    self.page_preference = shortcut # not found
-    """
-
+    @pytest.mark.skip
     @allure.story("Video Track Limitation")
-    def test_timeline_video_track_limitation(self, driver: AppiumU2Driver,
-                                             page_edit, page_main):
+    def test_timeline_video_track_limitation(self, driver: AppiumU2Driver):
         try:
+            allure.title(f'Add {self.VIDEO_LIMITATION} video(s) to PiP track')
             for i in range(self.VIDEO_LIMITATION):
-                allure.title('Add a video to PiP track')
-                page_edit.add_pip_media(media_type='Video')
+                self.page_edit.add_pip_media(media_type='Video')
 
-            allure.title('Add another video after reaching track limitation')
-            page_edit.add_pip_media(media_type='Video')
+            self.page_edit.add_pip_media(media_type='Video')
             allure.title('Should pop dialog if trying to add video while reaching track limitation')
             assert driver.get_text(L.import_media.device_limit.limit_title) == 'Video Overlay Maximum Exceeded'
-            driver.click_element(L.import_media.device_limit.btn_remind_ok)
+
+            self.click(L.import_media.device_limit.btn_remind_ok)
 
         except Exception as e:
-            self.exception_handler(e, driver, page_main)
+            self.exception_handler(e, driver, self.page_main)
 
     @allure.story("Audio Track Limitation")
-    def test_timeline_audio_track_limitation(self, driver: AppiumU2Driver,
-                                             page_edit, page_main, import_media):
+    def test_timeline_audio_track_limitation(self, driver: AppiumU2Driver):
+
+        def add_audio_track() -> None:
+            self.page_edit.enter_audio_library(audio_type='SFX')
+            self.click(L.import_media.library_listview.frame_song)
+            if self.elements(L.import_media.library_listview.download_song):
+                self.click(L.import_media.library_listview.download_song)
+            else:
+                self.click(L.import_media.library_listview.add)
+
+        def get_first_two_track_y_value() -> tuple:
+            """
+            Return the first two track y values, can use to check if the two tracks are in the same tracks
+            """
+            y1 = self.element(L.edit.timeline.clip_audio(index=1)).rect['y']  # Return int
+            y2 = self.element(L.edit.timeline.clip_audio(index=2)).rect['y']  # Return int
+            return y1, y2
 
         try:
-            page_edit.enter_audio_library(audio_type='SFX')
-            import_media.select_music_library()
-            time.sleep(20)
+            allure.title(f'Add {self.AUDIO_LIMITATION} audio(s) to PiP track')
+            for _ in range(self.AUDIO_LIMITATION):
+                add_audio_track()
+
+            # The ninth audio track should not be on the first audio track,
+            audio1_y, audio2_y = get_first_two_track_y_value()
+            assert audio1_y != audio2_y
+
+            # The tenth audio track should be on the first audio track
+            allure.title(f'The new audio should be on the '
+                         f'first track if already have {self.AUDIO_LIMITATION} audio tracks')
+            add_audio_track()
+            audio1_y, audio2_y = get_first_two_track_y_value()
+            assert audio1_y == audio2_y
 
         except Exception as e:
-            self.exception_handler(e, driver, page_main)
+            self.exception_handler(e, driver, self.page_main)
 
+    @pytest.mark.skip
     @allure.story("PiP Track Limitation")
-    def test_timeline_video_track_limitation(self, driver: AppiumU2Driver,
-                                             page_edit, page_main):
+    def test_timeline_video_track_limitation(self, driver: AppiumU2Driver):
 
         try:
             pass
 
         except Exception as e:
-            self.exception_handler(e, driver, page_main)
+            self.exception_handler(e, driver, self.page_main)
