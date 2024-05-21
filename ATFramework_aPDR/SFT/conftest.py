52f4ef404c43ffcb0b2e01ed9abf231594c5afab
yukino0113 <jethro0113@gmail.com>
2024-05-21 18:50:47 +0800

    Update file
    1. Add auto input device uuid
    2. Add stop app before test to prevent app not killed

--- ATFramework_aPDR/SFT/conftest.py
+++ ATFramework_aPDR/SFT/conftest.py
@@ -84,7 +84,7 @@ def driver():
         logger('**** Debug Mode ****')
         desired_caps['udid'] = '0B211JEC210389'
         if desired_caps['udid'] not in os.popen('adb devices').read():
-            desired_caps['udid'] = '0B211JEC210389'
+            desired_caps['udid'] = os.popen('adb devices').read().split('\n')[1].split('\t')[0]
             # S22 = 0B211JEC210389
             # P4a = R5CT21VMV6P
             # desired_caps['udid'] = '9596423546005V8'
@@ -146,6 +146,7 @@ def driver():
 @pytest.fixture(scope='class', autouse=True)
 def driver_init(driver):
     logger("[Start] Init driver session")
+    driver.stop_app(package_name)
     driver.activate_app(package_name)
     yield
     driver.stop_app(package_name)
