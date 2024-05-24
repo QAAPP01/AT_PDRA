a49860d9d50beb25d84fad04a77c4cf7257a6d6b
yukino0113 <jethro0113@gmail.com>
2024-05-24 09:33:15 +0800

    Reapply "Merge branch 'Develop_Jethro'"
    This reverts commit 7bf01f9b2a0decd8f867684c05bc438da91136c2.

---
+++ ATFramework_aPDR/SFT/Test_Master/TestReplace.py
@@ -0,0 +1,264 @@
+import time
+import pytest
+import allure
+
+from ATFramework_aPDR.ATFramework.utils import logger
+from ATFramework_aPDR.ATFramework.utils.compare_Mac import CompareImage
+import ATFramework_aPDR.pages.locator as L
+from ATFramework_aPDR.ATFramework.drivers.appium_driver import AppiumU2Driver
+from ATFramework_aPDR.SFT.Test_Timeline.TestBase import TestBase
+
+
+@pytest.fixture(scope='class', autouse=True)
+def class_setup(shortcut, driver):
+    driver.activate_app('com.cyberlink.powerdirector.DRA140225_01')
+    page_main, *_ = shortcut
+    page_main.enter_launcher()
+    page_main.subscribe()
+    page_main.enter_timeline()
+    yield
+    driver.stop_app('com.cyberlink.powerdirector.DRA140225_01')
+
+
+@allure.feature('Replace Video')
+class TestMasterReplaceVideo(TestBase):
+
+    @pytest.fixture(autouse=True)
+    def function_setup_teardown(self, shortcut, driver):
+        # shortcut
+        self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
+
+        self.click = self.page_main.h_click
+        self.element = self.page_main.h_get_element
+        self.elements = self.page_main.h_get_elements
+
+        self.page_main.enter_timeline(skip_media=False)
+        self.original_preview = self.page_edit.get_preview_pic()
+        yield
+        self.click(L.edit.menu.home)
+
+    @allure.story('Replace button function')
+    def test_video_replace_button(self, driver: AppiumU2Driver):
+        try:
+            driver.implicit_wait(5)
+            assert self.page_edit.select_from_bottom_edit_menu('Replace')
+            self.click(L.import_media.media_library.back)
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace video to video')
+    def test_replace_video_to_video(self, driver):
+        try:
+            self.page_edit.select_from_bottom_edit_menu('Replace')
+            self.page_media.select_media_by_order(order=2)
+            driver.implicit_wait(5)
+            if self.element(L.edit.replace.ok_btn):
+                self.click(L.edit.replace.ok_btn)
+            if self.element(L.edit.replace.btn_replace_anyway):
+                self.click(L.edit.replace.btn_replace_anyway)
+            assert not CompareImage(self.original_preview, self.page_edit.get_preview_pic(), 7).compare_image()
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace video to photo')
+    def test_replace_video_to_photo(self, driver):
+        try:
+            self.page_edit.select_from_bottom_edit_menu('Replace')
+            driver.implicit_wait(5)
+            self.page_media.switch_to_photo_library()
+            driver.implicit_wait(5)
+            self.page_media.select_media_by_order(order=2)
+            time.sleep(5)
+            assert not CompareImage(self.original_preview, self.page_edit.get_preview_pic(), 7).compare_image()
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace video to colorboard')
+    def test_replace_video_to_colorboard(self, driver):
+        try:
+            self.page_edit.select_from_bottom_edit_menu('Replace')
+            driver.implicit_wait(5)
+            self.page_media.switch_to_photo_library()
+            driver.implicit_wait(5)
+            self.page_media.select_media_by_order(order=2)
+            time.sleep(5)
+            assert not CompareImage(self.original_preview, self.page_edit.get_preview_pic(), 7).compare_image()
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+
+@allure.feature('Replace Photo')
+class TestMasterReplacePhoto(TestBase):
+
+    @pytest.fixture(autouse=True)
+    def function_setup_teardown(self, shortcut, driver):
+        # shortcut
+        self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
+
+        self.click = self.page_main.h_click
+        self.element = self.page_main.h_get_element
+        self.elements = self.page_main.h_get_elements
+
+        self.page_main.enter_timeline()
+        yield
+        self.click(L.edit.menu.home)
+
+    @allure.story('Replace photo to video')
+    def test_replace_photo_to_video(self):
+        try:
+            pass
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace photo to photo')
+    def test_replace_photo_to_photo(self):
+        try:
+            pass
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace photo to colorboard')
+    def test_replace_photo_to_colorboard(self):
+        try:
+            pass
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+
+@allure.feature('Replace Colorboard')
+class TestMasterReplaceColorbaord(TestBase):
+
+    @pytest.fixture(autouse=True)
+    def function_setup_teardown(self, shortcut, driver):
+        # shortcut
+        self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
+
+        self.click = self.page_main.h_click
+        self.element = self.page_main.h_get_element
+        self.elements = self.page_main.h_get_elements
+
+        self.page_main.enter_timeline()
+        yield
+        self.click(L.edit.menu.home)
+
+    @allure.story('Replace colorboard to video')
+    def test_replace_photo_to_video(self):
+        try:
+            pass
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace colorboard to photo')
+    def test_replace_photo_to_photo(self):
+        try:
+            pass
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+
+    @allure.story('Replace colorboard to video')
+    def test_replace_photo_to_colorboard(self):
+        try:
+            pass
+
+        except Exception as e:
+            if type(e) is AssertionError:
+                logger(f'[AssertionError] {repr(e)}]')
+                text = 'AssertionError'
+            elif type(e) is ValueError:
+                logger(f'[UnitTestError] {repr(e)}]')
+                text = 'UnitTestError'
+            else:
+                logger(f'[Exception] {repr(e)}')
+                text = 'Exception'
+            pytest.fail(f'[{text}] {repr(e)}]')
+            
--- ATFramework_aPDR/SFT/conftest.py
+++ ATFramework_aPDR/SFT/conftest.py
@@ -6,7 +6,6 @@ import os
 import pytest
 import sys
 from ATFramework_aPDR.ATFramework.utils.log import logger
-from ATFramework_aPDR.pages.page_factory import PageFactory
 from main_server_sacn import package_name
 from appium.webdriver.appium_service import AppiumService
 from selenium.common import InvalidSessionIdException
@@ -83,9 +82,11 @@ def driver():
 
     if debug_mode:
         logger('**** Debug Mode ****')
-        desired_caps['udid'] = 'R5CT32Q3WQN'
+        desired_caps['udid'] = '0B211JEC210389'
         if desired_caps['udid'] not in os.popen('adb devices').read():
-            desired_caps['udid'] = 'R5CW31G76ST'
+            desired_caps['udid'] = os.popen('adb devices').read().split('\n')[1].split('\t')[0]
+            # S22 = 0B211JEC210389
+            # P4a = R5CT21VMV6P
             # desired_caps['udid'] = '9596423546005V8'
 
         mode = 'debug'
@@ -145,18 +146,19 @@ def driver():
 @pytest.fixture(scope='class', autouse=True)
 def driver_init(driver):
     logger("[Start] Init driver session")
-    driver.driver.launch_app()
+    driver.stop_app(package_name)
+    driver.activate_app(package_name)
     yield
-    driver.driver.close_app()
+    driver.stop_app(package_name)
 
 
 @pytest.fixture(scope="session")
 def shortcut(driver):
-    page_main = PageFactory().get_page_object("main_page", driver)
-    page_edit = PageFactory().get_page_object("edit", driver)
-    page_media = PageFactory().get_page_object("import_media", driver)
-    page_preference = PageFactory().get_page_object("timeline_settings", driver)
-    return page_main, page_edit, page_media, page_preference
+    from ATFramework_aPDR.pages.edit import EditPage
+    from ATFramework_aPDR.pages.import_media import MediaPage
+    from ATFramework_aPDR.pages.main_page import MainPage
+    from ATFramework_aPDR.pages.timeline_settings import TimelineSettingsPage
+    return MainPage(driver), EditPage(driver), MediaPage(driver), TimelineSettingsPage(driver)
 
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
