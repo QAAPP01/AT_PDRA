6002f062c0afec32b83063751d44f83c006fda81
Hausen_Lin <hausen>
2024-06-13 16:35:58 +0800

    Shortcut AI Art

--- ATFramework_aPDR/ServerScan/test_scan_ai_style.py
+++ ATFramework_aPDR/ServerScan/test_scan_ai_style.py
@@ -4,7 +4,7 @@ import pytest
 import allure
 
 from ATFramework_aPDR.ATFramework.utils.compare_Mac import HCompareImg
-from ATFramework_aPDR.ATFramework.utils.log import logger
+
 from ATFramework_aPDR.pages.locator import locator as L
 from ATFramework_aPDR.pages.locator.locator_type import *
 from .conftest import TEST_MATERIAL_FOLDER as test_material_folder
@@ -55,7 +55,6 @@ class Test_Scan_AI_Art:
 
     @allure.title("Maid")
     def test_ai_art_maid(self, driver):
-
         try:
             self.page_main.enter_launcher()
             self.page_main.subscribe()
