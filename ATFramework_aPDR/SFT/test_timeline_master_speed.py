36d2bb23e10926e320b717054f512c7f47a35436
yukino0113 <jethro0113@gmail.com>
2024-06-18 15:56:42 +0800

    Update case
    1. Add step to currently case (40%)
    2. Fixed track limitation fail case

--- ATFramework_aPDR/SFT/test_timeline_master_speed.py
+++ ATFramework_aPDR/SFT/test_timeline_master_speed.py
@@ -14,24 +14,28 @@ original_video_length: float = 0
 class TestMasterVideoSpeed:
 
     @pytest.fixture(scope='class', autouse=True)
-    def class_setup(self, shortcut, driver):
+    def test_setup(self, shortcut, driver):
         global original_video_length
         page_main, page_edit, *_ = shortcut
-
-        page_main.enter_launcher()
         click = page_main.h_click
-        click(L.main.new_project)
-        click(L.import_media.media_library.media(index=1))
-        click(L.import_media.media_library.apply)
 
-        click(T.id('item_view_bg'))
-        page_edit.click_sub_tool('Speed')
+        with allure.step('[Step] Enter launcher'):
+            page_main.enter_launcher()
+        with allure.step('[Step] New timeline with video'):
+            click(L.main.new_project)
+            click(L.import_media.media_library.media(index=1))
+            click(L.import_media.media_library.apply)
+
+        with allure.step('[Step] Enter Speed function'):
+            click(T.id('item_view_bg'))
+            page_edit.click_sub_tool('Speed')
         original_video_length = page_main.element(T.id('item_view_bg')).rect['width']
         yield
-        page_edit.back_to_launcher()
+        with allure.step('[Step] Back to launcher'):
+            page_edit.back_to_launcher()
 
     @pytest.fixture(autouse=True)
-    def function_setup_teardown(self, shortcut, driver):
+    def define_vars(self, shortcut, driver):
         # shortcut
         self.page_main, self.page_edit, self.page_media, self.page_preference = shortcut
 
@@ -47,17 +51,20 @@ class TestMasterVideoSpeed:
 
     @allure.title('Speed max value')
     def test_max_speed(self):
-        self.element(L.edit.speed.slider).send_keys(9999)
+        with allure.step('[Set] Slider max value'):
+            self.element(L.edit.speed.slider).send_keys(9999)
         assert self.element(L.edit.speed.preview_toast_text).text == '8x'
 
     @allure.title('Speed min value')
     def test_min_speed(self):
-        self.element(L.edit.speed.slider).send_keys(-9999)
+        with allure.step('[Set] Slider min value'):
+            self.element(L.edit.speed.slider).send_keys(-9999)
         assert self.element(L.edit.speed.preview_toast_text).text == '0.125x'
 
     @allure.title('Set to smaller magnification')
     def test_slower_speed(self):
-        self.element(L.edit.speed.slider).send_keys(randint(0, 24))  # 1x is 25
+        with allure.step('[Set] Slider smaller value'):
+            self.element(L.edit.speed.slider).send_keys(randint(0, 24))  # 1x is 25
         assert float(self.element(L.edit.speed.preview_toast_text).text[:-1]) < 1
 
     @allure.title('Slower clip time should be correct')
@@ -67,7 +74,8 @@ class TestMasterVideoSpeed:
 
     @allure.title('Set to bigger magnification')
     def test_faster_speed(self):
-        self.element(L.edit.speed.slider).send_keys(randint(26, 100))  # 1x is 25
+        with allure.step('[Set] Slider bigger value'):
+            self.element(L.edit.speed.slider).send_keys(randint(26, 100))  # 1x is 25
         assert float(self.element(L.edit.speed.preview_toast_text).text[:-1]) > 1
 
     @allure.title('Faster clip time should be correct')
@@ -77,56 +85,66 @@ class TestMasterVideoSpeed:
 
     @allure.title('Mute button can be enable')
     def test_mute_enabled(self):
-        self.element(L.edit.speed.slider).send_keys(randint(26, 100))
-        mute = self.element(L.edit.speed.mute_audio)
-        mute.click()
+        with allure.step('[Set] Slider bigger value'):
+            self.element(L.edit.speed.slider).send_keys(randint(26, 100))
+        with allure.step('[Click] mute'):
+            mute = self.element(L.edit.speed.mute_audio)
+            mute.click()
         assert mute.get_attribute('selected') == 'true'
 
     @allure.title('Mute button can be disable')
     def test_mute_disabled(self):
-        mute = self.element(L.edit.speed.mute_audio)
-        mute.click()
+        with allure.step('[Click] mute'):
+            mute = self.element(L.edit.speed.mute_audio)
+            mute.click()
         assert mute.get_attribute('selected') == 'false'
 
     @allure.title('Keep pitch button can be enable')
     def test_keep_pitch_enabled(self):
-        keep_pitch = self.element(L.edit.speed.keep_pitch)
-        keep_pitch.click()
+        with allure.step('[Click] Keep pitch'):
+            keep_pitch = self.element(L.edit.speed.keep_pitch)
+            keep_pitch.click()
         assert keep_pitch.get_attribute('selected') == 'true'
 
     @allure.title('Keep pitch button can be disable')
     def test_keep_pitch_disabled(self):
-        keep_pitch = self.element(L.edit.speed.keep_pitch)
-        keep_pitch.click()
+        with allure.step('[Click] Keep pitch'):
+            keep_pitch = self.element(L.edit.speed.keep_pitch)
+            keep_pitch.click()
         assert keep_pitch.get_attribute('selected') == 'false'
 
     @allure.title('Ease in button can be enable')
     def test_ease_in_enabled(self):
-        ease_in = self.element(L.edit.speed.ease_in)
-        ease_in.click()
+        with allure.step('[Click] Ease in'):
+            ease_in = self.element(L.edit.speed.ease_in)
+            ease_in.click()
         assert ease_in.get_attribute('selected') == 'true'
 
     @allure.title('Ease in button can be disable')
     def test_ease_in_disabled(self):
-        ease_in = self.element(L.edit.speed.ease_in)
-        ease_in.click()
+        with allure.step('[Click] Ease in'):
+            ease_in = self.element(L.edit.speed.ease_in)
+            ease_in.click()
         assert ease_in.get_attribute('selected') == 'false'
 
     @allure.title('Ease out button can be enable')
     def test_ease_out_enabled(self):
-        ease_out = self.element(L.edit.speed.ease_out)
-        ease_out.click()
+        with allure.step('[Click] Ease out'):
+            ease_out = self.element(L.edit.speed.ease_out)
+            ease_out.click()
         assert ease_out.get_attribute('selected') == 'true'
 
     @allure.title('Ease out button can be disable')
     def test_ease_out_disabled(self):
-        ease_out = self.element(L.edit.speed.ease_out)
-        ease_out.click()
+        with allure.step('[Click] Ease out'):
+            ease_out = self.element(L.edit.speed.ease_out)
+            ease_out.click()
         assert ease_out.get_attribute('selected') == 'false'
 
     @allure.title('Tapping reset button can reset all option')
     def test_reset_button(self):
-        self.click(L.edit.speed.reset)
+        with allure.step('[Click] Reset'):
+            self.click(L.edit.speed.reset)
         assert self.element(L.edit.speed.preview_toast_text).text == '1x'
         assert self.element(L.edit.speed.mute_audio).get_attribute('selected') == 'false'
         assert self.element(L.edit.speed.keep_pitch).get_attribute('selected') == 'false'
@@ -135,7 +153,8 @@ class TestMasterVideoSpeed:
 
     @allure.title('Tapping reset button when not modified will not change settings')
     def test_reset_again(self):
-        self.click(L.edit.speed.reset)
+        with allure.step('[Click] Reset'):
+            self.click(L.edit.speed.reset)
         assert self.element(L.edit.speed.preview_toast_text).text == '1x'
         assert self.element(L.edit.speed.mute_audio).get_attribute('selected') == 'false'
         assert self.element(L.edit.speed.keep_pitch).get_attribute('selected') == 'false'
