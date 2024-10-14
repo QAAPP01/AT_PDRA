ff3380926c888185f398331faf6de8e4928d4974
Hausen_Lin <hausen>
2024-10-14 14:06:22 +0800

    [Hausen] Server Scan SFX

--- ATFramework_aPDR/ServerScan/test_scan_sfx.py
+++ ATFramework_aPDR/ServerScan/test_scan_sfx.py
@@ -21,41 +21,6 @@ class Test_Scan_SFX:
         self.is_exist = self.page_main.h_is_exist
         self.is_not_exist = self.page_main.h_is_not_exist
 
-    @allure.story("Getty")
-    @allure.title("Download")
-    def test_getty_music_download(self, driver):
-        func_name = inspect.stack()[0][3]
-        logger(f"\n[Start] {func_name}")
-
-        try:
-            self.page_main.enter_launcher()
-            self.page_main.enter_timeline()
-            self.page_edit.enter_audio_library('SFX')
-            self.page_media.click_sfx_tab('getty')
-
-            self.click(L.import_media.music_library.category(3))
-            music = self.elements(L.import_media.music_library.music(0))
-            for i in music:
-                i.click()
-                if self.click(L.import_media.music_library.download, 2):
-                    self.click(L.edit.try_before_buy.try_it_first, 1)
-                    if self.is_exist(L.import_media.music_library.download_cancel, 1):
-                        self.is_not_exist(L.import_media.music_library.download_cancel)
-                    break
-            assert self.is_exist(L.import_media.music_library.add)
-
-        except Exception as e:
-            traceback.print_exc()
-            driver.driver.close_app()
-            driver.driver.launch_app()
-
-            self.page_main.enter_launcher()
-            self.page_main.enter_timeline()
-            self.page_edit.enter_audio_library('SFX')
-            self.page_media.click_sfx_tab('getty')
-
-            pytest.fail(f"{str(e)}")
-
     @allure.story("Meta")
     @allure.title("Download")
     def test_meta_sfx_download(self, driver):
@@ -63,6 +28,8 @@ class Test_Scan_SFX:
         logger(f"\n[Start] {func_name}")
 
         try:
+            self.page_main.enter_timeline()
+            self.page_edit.enter_audio_library('SFX')
             self.page_media.click_sfx_tab('meta')
 
             music = self.elements(L.import_media.music_library.music(0))
