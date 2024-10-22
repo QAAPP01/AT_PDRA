b990218897acfe4f3110abecf7829a436d32db4c
Hausen_Lin <hausen>
2024-10-22 14:05:11 +0800

    [Framework] Credit dialog

--- ATFramework_aPDR/pages/main_page.py
+++ ATFramework_aPDR/pages/main_page.py
@@ -74,12 +74,12 @@ class MainPage(BasePage):
                 else:
                     # Churn Recovery
                     time.sleep(1)
-                    self.click(id('iv_close'), 1)
                     if self.h_is_exist(L.main.premium.pdr_premium, 1):
                         self.driver.driver.back()
                     else:
                         # IAP
                         self.click(L.main.premium.iap_back, 1)
+                        self.click(id('iv_close'), 2)
             if subscribe:
                 self.subscribe()
             logger('Enter Launcher Done')
