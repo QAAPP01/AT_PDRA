32dabcce78a1c83d8e074c9976b74ec2d904b831
Hausen_Lin <hausen>
2024-11-27 16:28:20 +0800

    [Hausen] Refine report compress file

--- send_mail/sendemail.py
+++ send_mail/sendemail.py
@@ -9,13 +9,6 @@ from email.mime.application import MIMEApplication
 from os.path import basename
 
 
-def compress_attachments(attachment_list, zip_filename="attachments.zip"):
-    with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for f in attachment_list:
-            zipf.write(f, basename(f))
-    return zip_filename
-
-
 def send_mail(opts):
     try:
         # Set info
@@ -34,15 +27,6 @@ def send_mail(opts):
         text = opts['text']
         html = opts['html']
         attachment = opts['attachment']
-
-        # Check the total size of attachments
-        total_size = sum(os.path.getsize(f) for f in attachment)
-        max_size = 25 * 1024 * 1024  # 25 MB
-
-        if total_size > max_size:
-            print(f"Attachments exceed {max_size / (1024 * 1024)} MB, compressing...")
-            zip_file = compress_attachments(attachment)
-            attachment = [zip_file]  # Replace with the zip file
 
         # Transfer data
         part1 = MIMEText(text, 'plain')
