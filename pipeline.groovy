b40608e977f47f9d61c08e784a73acc53ad36693
Hausen_Lin <hausen>
2024-12-27 14:13:00 +0800

    [Framework] Fix import send mail
    (cherry picked from commit f6727ed5ff63f2a65ece6ac04ad0699aabc6ead1)

--- pipeline.groovy
+++ pipeline.groovy
@@ -107,7 +107,7 @@ pipeline {
                     script {
                         echo "Generating Allure Report..."
                         try {
-                            allure includeProperties: false, jdk: '', results: [[path: 'sft-allure-results']], reportDirectory: 'sft-allure-report'
+                            allure includeProperties: false, jdk: '', results: [[path: 'sft-allure-results']]
                             env.reportUrl = "${env.BUILD_URL}allure"
                         } catch (Exception e) {
                             echo "Failed to generate Allure Report: ${e.message}"
