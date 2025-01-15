8b9d6c58f5c484e393c8e0681b8ce4c03da6e1c9
Hausen_Lin <hausen>
2025-01-15 10:08:58 +0800

    [Hausen] PIP limitation

--- ATFramework_aPDR/SFT/test_timeline_pip_track_limitation.py
+++ ATFramework_aPDR/SFT/test_timeline_pip_track_limitation.py
@@ -31,6 +31,13 @@ class Test_Track_Limitation:
         data = {'last_result': True}
         yield data
 
+    def last_is_fail(self, data):
+        if not data['last_result']:
+            data['last_result'] = True
+            self.page_main.relaunch()
+            return True
+        return False
+
     def get_root_element(self, xpath):
         try:
             page_source = self.driver.page_source
@@ -43,70 +50,107 @@ class Test_Track_Limitation:
     VIDEO_LIMITATION = 3
     AUDIO_LIMITATION = 9
     PIP_LIMITATION = 20
+    TRACKS_XPATH = '//*[contains(@resource-id, "tracks_container_of_not_main")]/android.widget.LinearLayout'
 
-    @allure.feature('Video')
+    @allure.feature('Track')
     @allure.story('Video')
     @allure.title('Track Limitation')
-    def test_timeline_limitation_video_track(self):
-        self.page_main.enter_timeline()
+    def test_timeline_limitation_video_track(self, data):
+        try:
+            with allure.step('Enter Timeline'):
+                self.page_main.enter_timeline()
 
-        for i in range(1, self.VIDEO_LIMITATION + 2):
-            with allure.step(f'[Step] Add {i} video tracks'):
-                self.page_edit.enter_main_tool('Overlay')
-                self.click(find_string('Video'))
-                self.click(L.import_media.media_library.media(index=1))
+            for i in range(1, self.VIDEO_LIMITATION + 2):
+                with allure.step(f'[Step] Add {i} video tracks'):
+                    self.page_edit.enter_main_tool('Overlay')
+                    self.click(find_string('Video'))
+                    self.click(L.import_media.media_library.media(index=1))
 
-        assert self.element(L.import_media.device_limit.limit_title).text == 'Video Overlay Maximum Exceeded'
+            assert self.element(L.import_media.device_limit.limit_title).text == 'Video Overlay Maximum Exceeded'
 
-        with allure.step('Close the limitation dialog'):
-            self.click(L.import_media.device_limit.btn_remind_ok)
+            with allure.step('Close the limitation dialog'):
+                self.click(L.import_media.device_limit.btn_remind_ok)
 
-        with allure.step('Back to launcher'):
-            self.page_edit.back_to_launcher()
+            with allure.step('Back to launcher'):
+                self.page_edit.back_to_launcher()
 
-    @allure.feature('Audio')
-    @allure.story('Music')
+        except Exception:
+            traceback.print_exc()
+            data['last_result'] = False
+            raise
+
+
+    @allure.feature('Track')
+    @allure.story('Audio')
     @allure.title('Track Limitation')
     def test_timeline_limitation_audio_track(self, data):
-        self.page_main.enter_timeline()
+        try:
+            if self.last_is_fail(data):
+                pass
+            with allure.step('Enter Timeline'):
+                self.page_main.enter_timeline()
 
-        TRACKS_XPATH = '//*[contains(@resource-id, "tracks_container_of_not_main")]/android.widget.LinearLayout'
+            for i in range(1, self.AUDIO_LIMITATION + 1):
+                with allure.step(f'[Step] Add {i} audio tracks'):
+                    self.page_edit.enter_audio_library(audio_type='SFX')
+                    if not self.elements(L.import_media.library_listview.add):
+                        self.click(L.import_media.library_listview.frame_song)
+                        self.click(L.import_media.library_listview.download_song)
+                    else:
+                        self.click(L.import_media.library_listview.add)
 
-        for i in range(1, self.AUDIO_LIMITATION + 1):
-            with allure.step(f'[Step] Add {i} audio tracks'):
+            target_elements = self.get_root_element(self.TRACKS_XPATH)
+            assert target_elements[-1].attrib['index'] == str(self.AUDIO_LIMITATION), f"Got {target_elements[-1].attrib['index']}"
+
+            with allure.step(f'[Step] Add 1 more audio'):
                 self.page_edit.enter_audio_library(audio_type='SFX')
                 if not self.elements(L.import_media.library_listview.add):
                     self.click(L.import_media.library_listview.frame_song)
                     self.click(L.import_media.library_listview.download_song)
                 else:
                     self.click(L.import_media.library_listview.add)
+                self.click(id('btn_session_back_icon'))
 
-        target_elements = self.get_root_element(TRACKS_XPATH)
-        assert target_elements[-1].attrib['index'] == str(self.AUDIO_LIMITATION), f"Got {target_elements[-1].attrib['index']}"
+            target_elements = self.get_root_element(self.TRACKS_XPATH)
+            assert target_elements[0].attrib['index'] == '1', f"Got {target_elements[0].attrib['index']}"
 
-        with allure.step(f'[Step] Add 1 more audio'):
-            self.page_edit.enter_audio_library(audio_type='SFX')
-            if not self.elements(L.import_media.library_listview.add):
-                self.click(L.import_media.library_listview.frame_song)
-                self.click(L.import_media.library_listview.download_song)
-            else:
-                self.click(L.import_media.library_listview.add)
-            self.click(id('btn_session_back_icon'))
+            with allure.step('Back to launcher'):
+                self.page_edit.back_to_launcher()
 
-        target_elements = self.get_root_element(TRACKS_XPATH)
-        assert target_elements[0].attrib['index'] == '1', f"Got {target_elements[0].attrib['index']}"
+        except Exception:
+            traceback.print_exc()
+            data['last_result'] = False
+            raise
 
-    @allure.feature('PiP')
-    @allure.story('Track Limitation')
-    def test_timeline_limitation_pip_track(self):
-        self.page_edit.enter_main_tool(name='Sticker')
-        self.click(find_string('Add Sticker'))
-        for i in range(1, self.PIP_LIMITATION + 2):
-            with allure.step(f'[Step] Add 1 pip, currently added {i} pip tracks'):
+    @allure.feature('Track')
+    @allure.story('PiP')
+    @allure.title('Track Limitation')
+    def test_timeline_limitation_pip_track(self, data):
+        try:
+            if self.last_is_fail(data):
+                pass
+            with allure.step('Enter Timeline'):
+                self.page_main.enter_timeline()
+
+            self.page_edit.enter_main_tool(name='Sticker')
+            for i in range(1, self.PIP_LIMITATION + 1):
+                with allure.step(f'[Step] Add {i} pip tracks'):
+                    self.click(L.edit.main_tool.sticker.item())
+            self.click(L.edit.pip.Text.back)
+
+            target_elements = self.get_root_element(self.TRACKS_XPATH)
+            assert target_elements[-1].attrib['index'] == str(self.PIP_LIMITATION), f"Got {target_elements[-1].attrib['index']}"
+
+            with allure.step(f'[Step] Add 1 more pip'):
+                self.page_edit.enter_main_tool(name='Sticker')
+                time.sleep(0.5)
                 self.click(L.edit.main_tool.sticker.item())
-        self.click(L.edit.pip.Text.back)
+                self.click(L.edit.pip.Text.back)
 
-        # Only the first track will have 2 pip clip
-        track = self.elements(id('item_view_bg'))
-        assert track[0].rect['y'] == track[1].rect['y']
-        assert track[2].rect['y'] != track[3].rect['y']
+            target_elements = self.get_root_element(self.TRACKS_XPATH)
+            assert target_elements[0].attrib['index'] == '0', f"Got {target_elements[0].attrib['index']}"
+
+        except Exception:
+            traceback.print_exc()
+            data['last_result'] = False
+            raise
